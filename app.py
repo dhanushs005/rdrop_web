1dc104d46901e025991c2d8da6029079e7644dd9
dhanushs005 <144521278+dhanushs005@users.noreply.github.com>
2025-08-06 21:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,7 @@ def download_video():
         shutil.rmtree(temp_dir, ignore_errors=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
